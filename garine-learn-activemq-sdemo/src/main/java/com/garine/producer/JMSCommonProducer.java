2592c235760274f6bca06841d57e1ca652c94ba6
garine95 <1039877898@qq.com>
2018-07-30 20:54:01 +0800

    注释整理

--- garine-learn-activemq-sdemo/src/main/java/com/garine/producer/JMSCommonProducer.java
+++ garine-learn-activemq-sdemo/src/main/java/com/garine/producer/JMSCommonProducer.java
@@ -8,12 +8,18 @@ public class JMSCommonProducer {
     public static void main(String[] args) throws JMSException {
         //根据broker URL建立连接工厂
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.0.15:61616");
+        //创建连接
         Connection connection = connectionFactory.createConnection();
+        //创建会话
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        //创建队列（有则不创建）
         Destination destination = session.createQueue("garine-queue");
+        //创建生产者
         MessageProducer producer = session.createProducer(destination);
+        //创建文本消息，有多种消息类型
         TextMessage textMessage = session.createTextMessage("Hello garine");
+        //发送消息
         producer.send(textMessage);
-        System.out.println("------------------over");
+        System.out.println("over");
     }
 }
--- garine-learn-dubbo-sdemo/src/main/java/com/garine/learn/dubbo/impl/Transporter$Adaptive.java
+++ garine-learn-dubbo-sdemo/src/main/java/com/garine/learn/dubbo/impl/Transporter$Adaptive.java
@@ -1,21 +1,42 @@
 package com.garine.learn.dubbo.impl;
 
 import com.alibaba.dubbo.common.extension.ExtensionLoader;
+
 public class Transporter$Adaptive implements com.alibaba.dubbo.remoting.Transporter {
     public com.alibaba.dubbo.remoting.Client connect(com.alibaba.dubbo.common.URL arg0, com.alibaba.dubbo.remoting.ChannelHandler arg1) throws com.alibaba.dubbo.remoting.RemotingException {
         if (arg0 == null) throw new IllegalArgumentException("url == null");
         com.alibaba.dubbo.common.URL url = arg0;
         String extName = url.getParameter("client", url.getParameter("transporter", "netty"));
-        if(extName == null) throw new IllegalStateException("Fail to get extension(com.alibaba.dubbo.remoting.Transporter) name from url(" + url.toString() + ") use keys([client, transporter])");
+        if (extName == null)
+            throw new IllegalStateException("Fail to get extension(com.alibaba.dubbo.remoting.Transporter) name from url(" + url.toString() + ") use keys([client, transporter])");
         com.alibaba.dubbo.remoting.Transporter extension = (com.alibaba.dubbo.remoting.Transporter) ExtensionLoader.getExtensionLoader(com.alibaba.dubbo.remoting.Transporter.class).getExtension(extName);
         return extension.connect(arg0, arg1);
     }
+
     public com.alibaba.dubbo.remoting.Server bind(com.alibaba.dubbo.common.URL arg0, com.alibaba.dubbo.remoting.ChannelHandler arg1) throws com.alibaba.dubbo.remoting.RemotingException {
         if (arg0 == null) throw new IllegalArgumentException("url == null");
         com.alibaba.dubbo.common.URL url = arg0;
         String extName = url.getParameter("server", url.getParameter("transporter", "netty"));
-        if(extName == null) throw new IllegalStateException("Fail to get extension(com.alibaba.dubbo.remoting.Transporter) name from url(" + url.toString() + ") use keys([server, transporter])");
-        com.alibaba.dubbo.remoting.Transporter extension = (com.alibaba.dubbo.remoting.Transporter)ExtensionLoader.getExtensionLoader(com.alibaba.dubbo.remoting.Transporter.class).getExtension(extName);
+        if (extName == null)
+            throw new IllegalStateException("Fail to get extension(com.alibaba.dubbo.remoting.Transporter) name from url(" + url.toString() + ") use keys([server, transporter])");
+        com.alibaba.dubbo.remoting.Transporter extension = (com.alibaba.dubbo.remoting.Transporter) ExtensionLoader.getExtensionLoader(com.alibaba.dubbo.remoting.Transporter.class).getExtension(extName);
         return extension.bind(arg0, arg1);
     }
+
+/*    public Object invokeMethod(Object o, String n, Class[] p, Object[] v) throws java.lang.reflect.InvocationTargetException {
+        com.garine.dubbo.api.HelloService w;
+        try {
+            w = ((com.garine.dubbo.api.HelloService) $1);
+        } catch (Throwable e) {
+            throw new IllegalArgumentException(e);
+        }
+        try {
+            if ("sayhi".equals($2) && $3.length == 1) {
+                return ($w) w.sayhi((java.lang.String) $4[0]);
+            }
+        } catch (Throwable e) {
+            throw new java.lang.reflect.InvocationTargetException(e);
+        }
+        throw new com.alibaba.dubbo.common.bytecode.NoSuchMethodException("Not found method \"" + $2 + "\" in class com.garine.dubbo.api.HelloService.");
+    }*/
 }
